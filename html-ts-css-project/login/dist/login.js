4ef15694beb62449b5199205b324808722513627
alexis <gog20@icloud.com>
2024-10-19 11:02:21 +0300

    av

---
+++ html-ts-css-project/login/dist/login.js
@@ -0,0 +1,4 @@
+var login = document.createElement('.log');
+login.innerText = "Welcome!/n Login";
+var loginContainer = document.getElementById('login-container');
+loginContainer.appendChild(login);
---
+++ html-ts-css-project/login/login.ts
@@ -0,0 +1,6 @@
+
+const login = document.createElement('.log') as HTMLParagraphElement;
+login.innerText="Welcome!/n Login";
+
+const loginContainer = document.getElementById('login-container')! as HTMLDivElement;
+loginContainer.appendChild(login);
