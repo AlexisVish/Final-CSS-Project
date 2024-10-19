f69c2b5f60548aee8ff7fe4bc0a245c9c56f3b4a
alexis <gog20@icloud.com>
2024-10-19 11:42:51 +0300

    av

--- html-ts-css-project/login/dist/login.js
+++ html-ts-css-project/login/dist/login.js
@@ -1,4 +1,39 @@
-var login = document.createElement('.log');
-login.innerText = "Welcome!/n Login";
-var loginContainer = document.getElementById('login-container');
-loginContainer.appendChild(login);
+var log = document.createElement("log");
+var loginContainer = document.getElementById("login-container");
+loginContainer.appendChild(log);
+log.className = 'log-box';
+var greet = document.createElement("p");
+var mailInput = document.createElement("mail");
+var passwordInput = document.createElement("password");
+var loginButton = document.createElement("button");
+greet.innerText = "Login";
+mailInput.placeholder = "enter your mail please";
+mailInput.className = 'input';
+passwordInput.placeholder = "enter your password please";
+passwordInput.className = 'input';
+loginButton.type = "submit";
+loginButton.textContent = "Login";
+loginButton.className = 'btn';
+log.append(greet, mailInput, passwordInput, loginButton);
+function checkingData(mailInput, passwordInput) {
+    try {
+        loginButton.addEventListener("click", function (event) {
+            if (mailInput.value === localStorage.getItem(mail.value) &&
+                passwordInput.value === localStorage.getItem(password.value)) {
+                return true;
+            }
+        });
+    }
+    catch (error) {
+        console.error("No data was found");
+    }
+}
+function login(checkingData) {
+    try {
+        if (checkingData === true) {
+        }
+    }
+    catch (error) {
+        console.error('No answer from checking function');
+    }
+}
--- html-ts-css-project/login/login.ts
+++ html-ts-css-project/login/login.ts
@@ -1,6 +1,51 @@
+const log = document.createElement("log") as HTMLParagraphElement;
 
-const login = document.createElement('.log') as HTMLParagraphElement;
-login.innerText="Welcome!/n Login";
+const loginContainer = document.getElementById(
+  "login-container"
+)! as HTMLDivElement;
+loginContainer.appendChild(log);
+log.className='log-box';
 
-const loginContainer = document.getElementById('login-container')! as HTMLDivElement;
-loginContainer.appendChild(login);
+const greet = document.createElement("p")! as HTMLParagraphElement;
+const mailInput = document.createElement("mail")! as HTMLInputElement;
+const passwordInput = document.createElement("password")! as HTMLInputElement;
+const loginButton = document.createElement("button")! as HTMLButtonElement;
+
+greet.innerText="Login";
+
+mailInput.placeholder="enter your mail please";
+mailInput.className='input';
+
+passwordInput.placeholder="enter your password please";
+passwordInput.className='input';
+
+loginButton.type="submit";
+loginButton.textContent="Login"
+loginButton.className='btn';
+
+log.append(greet, mailInput, passwordInput, loginButton);
+
+function checkingData(mailInput, passwordInput) {
+  try {
+    loginButton.addEventListener("click", (event) => {
+      if (
+        mailInput.value === localStorage.getItem(mail.value) &&
+        passwordInput.value === localStorage.getItem(password.value)
+      ) {
+        return true;
+      }
+    });
+  } catch (error) {
+    console.error("No data was found");
+  }
+}
+
+function login(checkingData){
+    try {
+        if (checkingData===true){
+
+        }
+    } catch (error) {
+        console.error('No answer from checking function')
+    }
+}
