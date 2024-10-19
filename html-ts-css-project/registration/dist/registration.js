6ba9aeb3d28d18d3131631b7abcdeedbe92417e8
alexis <gog20@icloud.com>
2024-10-19 15:56:28 +0300

    av

--- html-ts-css-project/registration/dist/registration.js
+++ html-ts-css-project/registration/dist/registration.js
@@ -1,2 +1,52 @@
+var Student = /** @class */ (function () {
+    function Student(n, p, em, pass) {
+        this.name = n;
+        this.phone = p;
+        this.email = em;
+        this.password = pass;
+    }
+    Student.prototype.getUser = function () {
+        localStorage.setItem("user", JSON.stringify(user));
+    };
+    return Student;
+}());
 var container = document.getElementById("registration-container");
-container.classList.add('box');
+container.classList.add("box");
+container.classList.add("open-sans");
+var reg = document.createElement('p');
+var form = document.createElement("form");
+var nameInput = document.createElement("input");
+var phoneInput = document.createElement("input");
+var mailInput = document.createElement("input");
+var passInput = document.createElement("input");
+var doublepass = document.createElement("input");
+var btns = document.createElement("div");
+var regButton = document.createElement("button");
+var backButton = document.createElement("button");
+reg.innerText = "Register!";
+reg.classList.add("h1");
+nameInput.placeholder = "Name";
+nameInput.type = "name";
+nameInput.classList.add("input");
+phoneInput.placeholder = "Phone";
+phoneInput.type = "phone";
+phoneInput.classList.add("input");
+mailInput.placeholder = "E-mail";
+mailInput.type = "mail";
+mailInput.classList.add("input");
+passInput.placeholder = "Password";
+passInput.type = "password";
+passInput.classList.add("input");
+doublepass.placeholder = "Repeat the password";
+doublepass.type = "password";
+doublepass.classList.add("input");
+regButton.textContent = "Register";
+regButton.classList.add("btn");
+backButton.textContent = "Back to login";
+backButton.classList.add("btn");
+btns.append(regButton, backButton);
+form.append(nameInput, phoneInput, mailInput, passInput, doublepass);
+container.append(reg, form, btns);
+var users = [];
+var user = new Student(nameInput.value, phoneInput.valueAsNumber, mailInput.value, passInput.value);
+users.push(user);
--- html-ts-css-project/registration/registration.ts
+++ html-ts-css-project/registration/registration.ts
@@ -1,2 +1,75 @@
-const container = document.getElementById("registration-container") as HTMLDivElement;
-container.classList.add('box');
+class Student {
+  name: string;
+  phone: number;
+  email: string;
+  password: string;
+  constructor(n: string, p: number, em: string, pass: string) {
+    this.name = n;
+    this.phone = p;
+    this.email = em;
+    this.password = pass;
+  }
+  getUser() {
+    localStorage.setItem("user", JSON.stringify(user));
+  }
+}
+
+const container = document.getElementById(
+  "registration-container"
+) as HTMLDivElement;
+container.classList.add("box");
+container.classList.add("open-sans");
+
+
+const reg = document.createElement('p')! as HTMLParagraphElement;
+const form = document.createElement("form")! as HTMLFormElement;
+const nameInput = document.createElement("input")! as HTMLInputElement;
+const phoneInput = document.createElement("input")! as HTMLInputElement;
+const mailInput = document.createElement("input")! as HTMLInputElement;
+const passInput = document.createElement("input")! as HTMLInputElement;
+const doublepass = document.createElement("input")! as HTMLInputElement;
+const btns = document.createElement("div")! as HTMLDivElement;
+const regButton = document.createElement("button")! as HTMLButtonElement;
+const backButton = document.createElement("button")! as HTMLButtonElement;
+
+reg.innerText="Register!";
+reg.classList.add("h1");
+
+nameInput.placeholder="Name";
+nameInput.type="name";
+nameInput.classList.add("input");
+
+phoneInput.placeholder="Phone";
+phoneInput.type="phone";
+phoneInput.classList.add("input");
+
+mailInput.placeholder="E-mail";
+mailInput.type="mail";
+mailInput.classList.add("input");
+
+passInput.placeholder="Password";
+passInput.type="password";
+passInput.classList.add("input");
+
+doublepass.placeholder="Repeat the password";
+doublepass.type="password";
+doublepass.classList.add("input");
+
+regButton.textContent="Register";
+regButton.classList.add("btn");
+
+
+backButton.textContent="Back to login"
+backButton.classList.add("btn");
+
+
+
+
+
+btns.append(regButton,backButton)
+form.append(nameInput, phoneInput, mailInput, passInput, doublepass);
+container.append(reg,form,btns);
+
+const users:Student[]=[];
+const user=new Student(nameInput.value,phoneInput.valueAsNumber,mailInput.value,passInput.value);
+users.push(user);
