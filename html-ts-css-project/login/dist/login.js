0e7ce2a9be9dad944da3e6e664530d7aa83885ab
alexis <gog20@icloud.com>
2024-10-19 13:03:10 +0300

    av

--- html-ts-css-project/login/dist/login.js
+++ html-ts-css-project/login/dist/login.js
@@ -1,8 +1,11 @@
-var log = document.createElement("log");
+var log = document.createElement("form");
 var loginContainer = document.getElementById("login-container");
 var greet = document.createElement("p");
-var mailInput = document.createElement("mail");
-var passwordInput = document.createElement("password");
+var mailInput = document.createElement("input");
+var passwordInput = document.createElement("input");
+var check = document.createElement("form");
+var checkbox = document.createElement("input");
+var label = document.createElement("label");
 var loginButton = document.createElement("button");
 greet.innerText = "Login";
 greet.classList.add("h1");
@@ -10,31 +13,40 @@ mailInput.type = "email";
 mailInput.placeholder = "Email";
 mailInput.classList.add("input");
 passwordInput.type = "password";
-passwordInput.placeholder = "Passsword";
+passwordInput.placeholder = "Password";
 passwordInput.classList.add("input");
+checkbox.type = "checkbox";
+checkbox.id = "rememberMe";
+label.htmlFor = checkbox.id;
+label.innerText = "Remember Me!";
+label.classList.add("smaller");
+check.classList.add("login-container");
+check.append(checkbox, label);
 loginButton.type = "submit";
-loginButton.textContent = "Login";
+loginButton.textContent = "Submit";
 loginButton.className = "btn";
 loginContainer.appendChild(log);
 log.className = "log-box";
 loginContainer.classList.add("open-sans");
-log.append(greet, mailInput, passwordInput, loginButton);
+log.append(greet, mailInput, passwordInput, check, loginButton);
 function checkingData(mailInput, passwordInput) {
     try {
         loginButton.addEventListener("click", function (event) {
-            if (mailInput.value === localStorage.getItem(mail.value) &&
-                passwordInput.value === localStorage.getItem(password.value)) {
-                return true;
-            }
+            event.preventDefault();
+            var mail = localStorage.getItem("mail");
+            var password = localStorage.getItem("password");
+            return mailInput.value === mail && passwordInput.value === password;
         });
     }
     catch (error) {
         console.error("No data was found");
     }
 }
-function login(checkingData) {
+var result = checkingData(mailInput, passwordInput);
+function login(result) {
     try {
-        if (checkingData === true) {
+        if (result === true) {
+            window.location.href = "/html-ts-css-project/home/home.html";
         }
     }
     catch (error) {
--- html-ts-css-project/login/login.ts
+++ html-ts-css-project/login/login.ts
@@ -1,54 +1,63 @@
-const log = document.createElement("log") as HTMLFormElement;
+const log = document.createElement("form") as HTMLFormElement;
 
 const loginContainer = document.getElementById(
   "login-container"
 )! as HTMLDivElement;
 
-
 const greet = document.createElement("p")! as HTMLParagraphElement;
-const mailInput = document.createElement("mail")! as HTMLInputElement;
-const passwordInput = document.createElement("password")! as HTMLInputElement;
+const mailInput = document.createElement("input")! as HTMLInputElement;
+const passwordInput = document.createElement("input")! as HTMLInputElement;
+const check = document.createElement("form")! as HTMLFormElement;
+const checkbox = document.createElement("input") as HTMLInputElement;
+const label = document.createElement("label")! as HTMLLabelElement;
 const loginButton = document.createElement("button")! as HTMLButtonElement;
 
 greet.innerText = "Login";
 greet.classList.add("h1");
 
-mailInput.type="email";
+mailInput.type = "email";
 mailInput.placeholder = "Email";
 mailInput.classList.add("input");
 
-passwordInput.type="password";
-passwordInput.placeholder = "Passsword";
+passwordInput.type = "password";
+passwordInput.placeholder = "Password";
 passwordInput.classList.add("input");
 
-loginButton.type = "submit";
-loginButton.textContent = "Login";
-loginButton.className = "btn";
+checkbox.type = "checkbox";
+checkbox.id = "rememberMe";
+label.htmlFor = checkbox.id;
+label.innerText = "Remember Me!";
+label.classList.add("smaller");
 
+check.classList.add("login-container");
+check.append(checkbox, label);
+
+loginButton.type = "submit";
+loginButton.textContent = "Submit";
+loginButton.className = "btn";
 
 loginContainer.appendChild(log);
 log.className = "log-box";
 loginContainer.classList.add("open-sans");
-log.append(greet, mailInput, passwordInput, loginButton);
+log.append(greet, mailInput, passwordInput, check, loginButton);
 
 function checkingData(mailInput, passwordInput) {
   try {
     loginButton.addEventListener("click", (event) => {
-      if (
-        mailInput.value === localStorage.getItem(mail.value) &&
-        passwordInput.value === localStorage.getItem(password.value)
-      ) {
-        return true;
-      }
+      event.preventDefault();
+      const mail = localStorage.getItem("mail");
+      const password = localStorage.getItem("password");
+      return mailInput.value === mail && passwordInput.value === password;
     });
   } catch (error) {
     console.error("No data was found");
   }
 }
-
-function login(checkingData) {
+const result = checkingData(mailInput, passwordInput);
+function login(result) {
   try {
-    if (checkingData === true) {
+    if (result === true) {
+      window.location.href = "/html-ts-css-project/home/home.html";
     }
   } catch (error) {
     console.error("No answer from checking function");
