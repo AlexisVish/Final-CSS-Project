43b01805e800c2bda944ce9a5f6b1e5ef66dbd55
Alexis <gog20@icloud.com>
2024-10-23 19:48:01 +0300

    av

--- html-ts-css-project/home/dist/home.js
+++ html-ts-css-project/home/dist/home.js
@@ -1,4 +1,7 @@
 var home = document.querySelector("#home");
 // const nav = document.createElement("nav") as HTMLElement;
 // const logo = document.createElement("");
-home.innerHTML = "\n  <div class=\"nav-bar box\">\n        <button class=\"nav-bar-account\">My Account</button>\n        <button class=\"nav-bar-courses\">My Courses</button>\n        <button class=\"nav-bar-zoom\">My Zoom Meetings</button>\n        <button class=\"nav-bar-forums\">My Forums</button>\n        <button class=\"nav-bar-lessons\">My Lessons</button>\n\n    </div>\n";
+home.innerHTML = "\n  <div class=\"nav-bar box\">\n        <button class=\"nav-bar-account\" onClick=\"onMyAccountClick()\">My Account</button>\n        <button class=\"nav-bar-courses\">My Courses</button>\n        <button class=\"nav-bar-zoom\">My Zoom Meetings</button>\n        <button class=\"nav-bar-forums\">My Forums</button>\n        <button class=\"nav-bar-lessons\">My Lessons</button>\n    </div>\n";
+function onMyAccountClick() {
+    window.location.href = "/html-ts-css-project/home/home.html";
+}
--- html-ts-css-project/home/home.ts
+++ html-ts-css-project/home/home.ts
@@ -5,13 +5,15 @@ const home = document.querySelector("#home")! as HTMLDivElement;
 
 home.innerHTML=`
   <div class="nav-bar box">
-        <button class="nav-bar-account">My Account</button>
+        <button class="nav-bar-account" onClick="onMyAccountClick()">My Account</button>
         <button class="nav-bar-courses">My Courses</button>
         <button class="nav-bar-zoom">My Zoom Meetings</button>
         <button class="nav-bar-forums">My Forums</button>
         <button class="nav-bar-lessons">My Lessons</button>
-
     </div>
 `
 
 
+function onMyAccountClick(){
+    window.location.href ="/html-ts-css-project/home/home.html"
+}
