145a5f032f9f63a31da971f13853dbcf91dda88e
Alexis <gog20@icloud.com>
2024-10-23 22:33:00 +0300

    av

--- html-ts-css-project/home/dist/home.js
+++ html-ts-css-project/home/dist/home.js
@@ -1,11 +1,16 @@
 var home = document.querySelector("#home");
-home.innerHTML = "\n  <div class=\"nav-bar box\">\n        <button class=\"nav-bar-account\" onClick=\"onMyAccountClick()\">My Account</button>\n        <button class=\"nav-bar-courses\">My Courses</button>\n        <button class=\"nav-bar-zoom\">My Zoom Meetings</button>\n        <button class=\"nav-bar-forums\">My Forums</button>\n        <button class=\"nav-bar-lessons\">My Lessons</button>\n    </div>\n   <input type=\"text\" class=\"text\" id=\"1\" placeholder=\"viva\" />\n\n   \n   ";
+var header = document.createElement("header");
+var navBar = document.createElement("div");
+home.appendChild(navBar);
+home.appendChild(header);
+navBar.innerHTML = "\n  <div class=\"nav-bar box\">\n        <button class=\"nav-bar-account\" onClick=\"onMyAccountClick()\">My Account</button>\n        <button class=\"nav-bar-courses\">My Courses</button>\n        <button class=\"nav-bar-zoom\">My Zoom Meetings</button>\n        <button class=\"nav-bar-forums\">My Forums</button>\n        <button class=\"nav-bar-lessons\">My Lessons</button>\n    </div>\n\n   \n   ";
+header.innerHTML = "\n   <div class=\"search-container\">\n        <input type=\"text\" id=\"search-bar\" placeholder=\"Search...\" />\n        <button id=\"search-button\">Search</button>\n    </div>\n   ";
 function onMyAccountClick() {
     window.location.href = "/html-ts-css-project/registration/registration.html";
 }
-var input = document.querySelector(".text");
-input.addEventListener("input", function () {
-    setTimeout(function () {
-        console.log(input.value);
-    }, 2000);
-});
+// const input = document.querySelector(".text")! as HTMLInputElement;
+// input.addEventListener("input", () => {
+//     setTimeout(() => {
+//         console.log(input.value);
+//     }, 2000);
+// });
--- html-ts-css-project/home/home.ts
+++ html-ts-css-project/home/home.ts
@@ -1,6 +1,10 @@
 const home = document.querySelector("#home")! as HTMLDivElement;
+const header = document.createElement("header") as HTMLHeadElement;
+const navBar = document.createElement("div")! as HTMLDivElement;
+home.appendChild(navBar);
+home.appendChild(header);
 
-home.innerHTML = `
+navBar.innerHTML = `
   <div class="nav-bar box">
         <button class="nav-bar-account" onClick="onMyAccountClick()">My Account</button>
         <button class="nav-bar-courses">My Courses</button>
@@ -8,20 +12,31 @@ home.innerHTML = `
         <button class="nav-bar-forums">My Forums</button>
         <button class="nav-bar-lessons">My Lessons</button>
     </div>
-   <input type="text" class="text" id="1" placeholder="viva" />
 
    
    `;
-   
+
+
+
+   header.innerHTML=`
+   <div class="search-container">
+        <input type="text" id="search-bar" placeholder="Search..." />
+        <button id="search-button">Search</button>
+    </div>
+   `
+
+
+
+
    function onMyAccountClick() {
        window.location.href = "/html-ts-css-project/registration/registration.html";
     }
     
-    const input = document.querySelector(".text")! as HTMLInputElement;
+    // const input = document.querySelector(".text")! as HTMLInputElement;
     
-    input.addEventListener("input", () => {
-        setTimeout(() => {
-            console.log(input.value);
-        }, 2000);
-    });
+    // input.addEventListener("input", () => {
+    //     setTimeout(() => {
+    //         console.log(input.value);
+    //     }, 2000);
+    // });
         
