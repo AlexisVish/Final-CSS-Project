ad51a9820063f69580be544caac5472fad2cc024
Alexis <gog20@icloud.com>
2024-10-23 19:59:38 +0300

    av

--- html-ts-css-project/home/dist/home.js
+++ html-ts-css-project/home/dist/home.js
@@ -1,7 +1,11 @@
 var home = document.querySelector("#home");
-// const nav = document.createElement("nav") as HTMLElement;
-// const logo = document.createElement("");
-home.innerHTML = "\n  <div class=\"nav-bar box\">\n        <button class=\"nav-bar-account\" onClick=\"onMyAccountClick()\">My Account</button>\n        <button class=\"nav-bar-courses\">My Courses</button>\n        <button class=\"nav-bar-zoom\">My Zoom Meetings</button>\n        <button class=\"nav-bar-forums\">My Forums</button>\n        <button class=\"nav-bar-lessons\">My Lessons</button>\n    </div>\n";
+home.innerHTML = "\n  <div class=\"nav-bar box\">\n        <button class=\"nav-bar-account\" onClick=\"onMyAccountClick()\">My Account</button>\n        <button class=\"nav-bar-courses\">My Courses</button>\n        <button class=\"nav-bar-zoom\">My Zoom Meetings</button>\n        <button class=\"nav-bar-forums\">My Forums</button>\n        <button class=\"nav-bar-lessons\">My Lessons</button>\n    </div>\n   <input type=\"text\" class=\"text\" id=\"1\" placeholder=\"viva\" />\n\n   \n   ";
 function onMyAccountClick() {
-    window.location.href = "/html-ts-css-project/home/home.html";
+    window.location.href = "/html-ts-css-project/registration/registration.html";
 }
+var input = document.querySelector(".text");
+input.addEventListener("input", function () {
+    setTimeout(function () {
+        console.log(input.value);
+    }, 2000);
+});
--- html-ts-css-project/home/home.ts
+++ html-ts-css-project/home/home.ts
@@ -1,9 +1,6 @@
 const home = document.querySelector("#home")! as HTMLDivElement;
 
-// const nav = document.createElement("nav") as HTMLElement;
-// const logo = document.createElement("");
-
-home.innerHTML=`
+home.innerHTML = `
   <div class="nav-bar box">
         <button class="nav-bar-account" onClick="onMyAccountClick()">My Account</button>
         <button class="nav-bar-courses">My Courses</button>
@@ -11,9 +8,20 @@ home.innerHTML=`
         <button class="nav-bar-forums">My Forums</button>
         <button class="nav-bar-lessons">My Lessons</button>
     </div>
-`
+   <input type="text" class="text" id="1" placeholder="viva" />
 
-
-function onMyAccountClick(){
-    window.location.href ="/html-ts-css-project/home/home.html"
-}
+   
+   `;
+   
+   function onMyAccountClick() {
+       window.location.href = "/html-ts-css-project/registration/registration.html";
+    }
+    
+    const input = document.querySelector(".text")! as HTMLInputElement;
+    
+    input.addEventListener("input", () => {
+        setTimeout(() => {
+            console.log(input.value);
+        }, 2000);
+    });
+        
