432123dae9d7b8f45b9a781e09f05aba0223f9c4
Alexis <gog20@icloud.com>
2024-11-30 11:39:16 +0200

    av

--- html-ts-css-project/main/dist/main.js
+++ html-ts-css-project/main/dist/main.js
@@ -1,25 +1,25 @@
 var main = document.getElementById("main");
 var conatiner = document.createElement("div");
 main.appendChild(conatiner);
-conatiner.classList.add('box');
-conatiner.classList.add('open-sans');
+conatiner.classList.add("box");
+conatiner.classList.add("open-sans");
 var greed = document.createElement("p");
 var btns = document.createElement("div");
 var regButton = document.createElement("button");
 var logButton = document.createElement("button");
 greed.innerText = "Welcome!";
 regButton.textContent = "Register";
-regButton.classList.add('btn');
+regButton.classList.add("btn");
 logButton.textContent = "Login";
-logButton.classList.add('btn');
+logButton.classList.add("btn");
 btns.className = "btns";
 btns.append(logButton, regButton);
 conatiner.append(greed, btns);
-logButton.addEventListener('click', function (event) {
+logButton.addEventListener("click", function (event) {
     event.preventDefault();
     window.location.href = "/html-ts-css-project/login/login.html";
 });
-regButton.addEventListener('click', function (event) {
+regButton.addEventListener("click", function (event) {
     event.preventDefault();
     window.location.href = "/html-ts-css-project/registration/registration.html";
 });
--- html-ts-css-project/main/main.ts
+++ html-ts-css-project/main/main.ts
@@ -2,31 +2,31 @@ const main = document.getElementById("main")! as HTMLDivElement;
 const conatiner = document.createElement("div")! as HTMLDivElement;
 main.appendChild(conatiner);
 
-conatiner.classList.add('box');
-conatiner.classList.add('open-sans');
+conatiner.classList.add("box");
+conatiner.classList.add("open-sans");
 
 const greed = document.createElement("p")! as HTMLParagraphElement;
 const btns = document.createElement("div")! as HTMLDivElement;
 const regButton = document.createElement("button")! as HTMLButtonElement;
 const logButton = document.createElement("button")! as HTMLButtonElement;
 
-greed.innerText=`Welcome!`;
+greed.innerText = `Welcome!`;
 
 regButton.textContent = "Register";
-regButton.classList.add('btn');
+regButton.classList.add("btn");
 
 logButton.textContent = "Login";
-logButton.classList.add('btn');
+logButton.classList.add("btn");
 
-btns.className="btns";
-btns.append(logButton,regButton);
-conatiner.append(greed,btns);
+btns.className = "btns";
+btns.append(logButton, regButton);
+conatiner.append(greed, btns);
 
-logButton.addEventListener('click', (event)=>{
-    event.preventDefault();
-window.location.href="/html-ts-css-project/login/login.html"
-})
-regButton.addEventListener('click', (event)=>{
-    event.preventDefault();
-window.location.href="/html-ts-css-project/registration/registration.html"
-})
+logButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  window.location.href = "/html-ts-css-project/login/login.html";
+});
+regButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  window.location.href = "/html-ts-css-project/registration/registration.html";
+});
